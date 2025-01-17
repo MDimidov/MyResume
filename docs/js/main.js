b9ad1eb4e135973cdbf0c6d4d9b0610d037b403d
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-01-17 13:37:56 +0200

    Remove unnecessary comment code

--- docs/js/main.js
+++ docs/js/main.js
@@ -73,14 +73,14 @@
     { offset: "80%" }
   );
 
-  // Porfolio isotope and filter
+  // Portfolio isotope and filter
   var portfolioIsotope = $(".portfolio-container").isotope({
     itemSelector: ".portfolio-item",
     layoutMode: "fitRows",
   });
 
-  $("#portfolio-flters li").on("click", function () {
-    $("#portfolio-flters li").removeClass("filter-active");
+  $("#portfolio-filters li").on("click", function () {
+    $("#portfolio-filters li").removeClass("filter-active");
     $(this).addClass("filter-active");
 
     portfolioIsotope.isotope({ filter: $(this).data("filter") });
